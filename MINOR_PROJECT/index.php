9bc743e5b942bd67886b702ab29dbfaa6a0b1457
Puja Bhattacharjee <60750190+pujabhattacharjee835@users.noreply.github.com>
2021-10-02 22:12:40 +0530

    Add files via upload

---
+++ MINOR_PROJECT/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body style="background-image: url('http://localhost/MINOR_PROJECT/images/back.jpg'); background-repeat: no-repeat; background-attachment: fixed; ">
+
+ <div class="div1">
+   	<a href=""><p>User Registration</p></a>
+   	<a href=""><p>User Login</p></a>
+   	<a href=""><p>Vendor Login</p></a>
+   	<a href=""><p>Admin Login</p></a>
+   </ul>
+</div>   
+   
+	
+
+</body>
+</html>
