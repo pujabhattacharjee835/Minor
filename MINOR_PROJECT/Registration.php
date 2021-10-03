953ae43a7ccc62b62400ea643d9b90be53f7c772
Puja Bhattacharjee <60750190+pujabhattacharjee835@users.noreply.github.com>
2021-10-03 18:41:33 +0530

    Add files via upload

---
+++ MINOR_PROJECT/Registration.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="CSS/style.css?v=<?php echo time(); ?>"> 
+	<meta charset="utf-8">
+	<title>Registration</title>
+</head>
+<body style="background:linear-gradient(90deg, #000046 0%, #1cb5e0 100%)">
+
+<div class="reg-form">
+
+	<form action="" method="">
+		<h1 class="heading1">Fill In The Form To Register</h1>
+		
+		<input type="text" name="reg_id" placeholder="Registration No." id="reg_no"><br><br>
+		
+		<input type="password" name="password" placeholder="Password" id="pass"><br><br>
+		
+		<input type="text" name="name" placeholder="Name" id="name"><br><br>
+		
+		<input type="text" name="phone" placeholder="Phone No." id="phone"><br><br>
+
+		<input type="submit" name="submit" value="SUBMIT" id="submit1">
+	</form>	
+</div>
+
+</body>
+</html>
---
+++ MINOR_PROJECT/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="CSS/style.css?v=<?php time(); ?>">
+	<meta charset="utf-8">
+	<title>LOGIN FORM</title>
+</head>
+<body style="background:linear-gradient(90deg, #000046 0%, #1cb5e0 100%)">
+
+	<div class="login-form">
+
+	<form action="" method="">
+		<h1 class="heading1">Login</h1>
+		
+		<input type="text" name="reg_id" placeholder="Registration No." id="reg_no"><br><br>
+		
+		<input type="password" name="password" placeholder="Password" id="pass"><br><br>
+		
+
+		<input type="submit" name="submit" value="SUBMIT" id="submit1">
+	</form>	
+</div>
+
+</body>
+</html>
